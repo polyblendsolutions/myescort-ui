60f7acd3d12397ce2bbdbd141b9f4fedecd7f0e4
polyblendsolutions <polyblend.solution@gmail.com>
2024-04-13 15:28:44 +0530

    Feat:Add vip status appearence on Vip user

--- src/app/shared/components/marketplace-card/marketplace-card.component.ts
+++ src/app/shared/components/marketplace-card/marketplace-card.component.ts
@@ -17,6 +17,7 @@ export class MarketplaceCardComponent implements OnInit {
   @Output() onClickCard = new EventEmitter<any>();
   tesData: any;
   isQaHover: boolean = false;
+  vipStatus: boolean = true;
 
   /***
    * Subscription
